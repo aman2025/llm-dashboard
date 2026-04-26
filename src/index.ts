b6de12a0ea64a880caa4f341779e74d20b42c02e
aman2025 <2247367564@qq.com>
2026-04-26 22:30:25 +0800

    添加生产和开发环境的env文件。

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+console.log("Environment:", process.env.NODE_ENV);
+console.log("PORT:", process.env.PORT);
+console.log("DATABASE_URL:", process.env.DATABASE_URL);
+console.log("AI_KEY:", process.env.AI_KEY);
+
+const app = new Elysia().get("/", () => "Hello Elysia").listen(Number(process.env.PORT) || 3002);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
